7ab978e903e542bc4567ed79fbee5e8e702dda24
Phuonganhnguyenngoc <107661142+Phuonganhnguyenngoc@users.noreply.github.com>
2022-06-17 09:53:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ const CONFIG = {
     title: 'nhắn hay cọc thôi chứ tớ rất hiền',
     desc: 'Con đường sắp tới mình sẽ đồng hành với nhau vui vẻ',
     btnYes: '...',
-    btnNo: 'chỗ này t ko biết viết gì ơ, viết lắm ngta lại tưởng t tự kỷ đọc thoại nội tâm :3',
-    question: 'năm cuối cấp rồi pa mong mi sẽ cố gắng học hơn,cái gì không tốt thì mình nên bỏ bớt ưu tiên cho nó lại, tương lai của m là qt nhất mà phải ko?',
+    btnNo: 'chỗ này t ko biết viết gì ơ, viết lắm người ta lại tưởng t tự kỷ đọc thoại nội tâm :3',
+    question: 'năm cuối cấp rồi pa mong mi sẽ cố gắng học hơn,cái gì không tốt thì mình nên bỏ bớt ưu tiên cho nó lại, tương lai của m là quan trọng nhất mà phải ko?',
     btnReply: 'Gửi cho bạn một trái tym to bự để động viên nà <3',
     reply: 'Chạy từ từ thuiii, chưa hết còn nữa...',
     mess: 'Pa cũng sẽ bên cạnh giúp mi, t không giỏi nhưng hứa sẽ cố gắng',
-    messDesc: 'Có t ở đây rồi, có gì khó khăn buồn bực t sẽ luôn lắng nghe.. cơ mà lâu lâu mới hiểu ấy',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Có t ở đây rồi, có gì khó khăn buồn bực t sẽ luôn lắng nghe.. cơ mà lâu lâu mới hiểu ấy. cũng cảm ơn rất nhiều vì đã tin tưởng taoo <3',
+    btnAccept: '.',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD:https://www.facebook.com/phuonganh.nguyenngoc.796569
 }
